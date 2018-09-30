ae089174965f3c7dc185ddfee55664368b561890
kahyeelee <kahyeelee@gmail.com>
2018-09-30 19:50:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,8 @@ def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
 
-names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
-def batch_badge_creator(names)
+def batch_badge_creator
+  puts "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"
 end
 
 names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
