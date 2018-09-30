11cc8a0e837d62863276b845f4f7726d8460fb66
kahyeelee <kahyeelee@gmail.com>
2018-09-30 19:42:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,9 +8,9 @@ def batch_badge_creator(names)
 end
 
 names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-room_number = ["1","2","3","4","5","6","7"]
 
-def assign_rooms(names, room_number)
+
+def assign_rooms(names)
   puts "Hello"
 
 
